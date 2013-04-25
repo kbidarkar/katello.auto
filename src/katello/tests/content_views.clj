06c854e17b3cba77366e14484ce9c1a732d11a27
Kedar Bidarkar <kbidarka@redhat.com>
2013-04-25 16:33:12 +0530

    Fixed the tests approach

--- src/katello/tests/content_views.clj
+++ src/katello/tests/content_views.clj
@@ -23,18 +23,16 @@
 
 
 (defgroup cv-tests
-  (deftest "Remove complete product or a repo from content-view-defnition"  
-     (with-unique [org (kt/newOrganization {:name "auto-org"})
-                   published-name ["pub-name1" "pub-name2"] 
-                   cv (kt/newContentView {:name "composite-view"
-                                          :org org 
-                                          :description "Composite Content View" 
-                                          :published-names [published-name]})
-                   modified-name "mod-name"]
-  
-       (let [repo (fresh-repo org "http://repos.fedorapeople.org/repos/pulp/pulp/v2/stable/6Server/")]
-         (ui/create-all (list org (kt/provider repo) (kt/product repo) repo cv)
-         (ui/update cv assoc :name "mod-name123" :description "modified description")               
+  (deftest "Remove complete product or a repo from content-view-defnition"
+    (let [org (uniqueify (kt/newOrganization {:name "auto-org"})) 
+          repo (fresh-repo org "http://repos.fedorapeople.org/repos/pulp/pulp/v2/stable/6Server/")
+          published-names (take 2 (uniques "pub-name"))
+          cv (uniqueify (kt/newContentView {:name "composite-view"
+                                           :org org 
+                                           :description "Composite Content View"}))
+          modified-name (uniqueify "mod-name")]
+         (ui/create-all (list org (kt/provider repo) (kt/product repo) repo cv))
+         ;;(ui/update cv assoc :name "mod-name123" :description "modified description")               
          ;;(ui/update (ui/update cv assoc :content repo) dissoc :content))))))
 ;;or
-         (-> cv (ui/update assoc :content repo) (ui/update dissoc :content)))))))
+         (-> cv (ui/update assoc :content (list repo)) (ui/update dissoc :content)))))
